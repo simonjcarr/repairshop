eceeee68c1fa82825f395268eb85db123bc1efaa
Simon Carr <simon@soxprox.com>
2024-10-23 21:22:30 +0100

    integrate kinde into the app

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,7 @@
-import { HomeIcon, File, UsersRound } from 'lucide-react';
+import { HomeIcon, File, UsersRound, LogOut } from 'lucide-react';
 import Link from 'next/link';
+import { LogoutLink } from '@kinde-oss/kinde-auth-nextjs/components'
+import { Button } from '@/components/ui/button';
 
 import { NavButton } from '@/components/NavButton';
 import { ModeToggle } from '@/components/ModeTopggle';
@@ -20,6 +22,17 @@ export function Header() {
                     <NavButton href='/tickets' label='Tickets' icon={File} />
                     <NavButton href='/customers' label='Customers' icon={UsersRound} />
                     <ModeToggle />
+                    <Button
+                        variant='ghost'
+                        size='icon'
+                        aria-label='Logout'
+                        className='rounded-full'
+                        asChild
+                    >
+                        <LogoutLink>
+                            <LogOut />
+                        </LogoutLink>
+                    </Button>
                 </div>
             </div>
         </header>
